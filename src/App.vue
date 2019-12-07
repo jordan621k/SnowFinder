8dabd8d0c7eed11882bb3bfda98cd67dc654167c
jordan621k <jordan621k@gmail.com>
2019-12-07 18:11:30 -0500

    Add map (#5)
    * adding open street map
    
    * add title img

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <v-app>
-      <Stats />
+    <img src="./assets/title.png">
+    <Stats />
+    <Map />
   </v-app>
 </template>
 
 <script>
 import Stats from './components/Stats';
-
+import Map from './components/Map';
 export default {
   name: 'App',
 
   components: {
     Stats,
+    Map
   },
 
   data: () => ({
   }),
 };
 </script>
+<style scoped>
+img {
+  width: 300px;
+  height: 150px;
+}
+</style>
