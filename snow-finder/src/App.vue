56cb175b512c76d4d732deaa014e365ca19d1e62
yomancool <tp65l4m06cl3@gmail.com>
2019-12-07 17:03:29 -0500

    adding open street map

--- snow-finder/src/App.vue
+++ snow-finder/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <v-app>
       <Stats />
+      <Map />
   </v-app>
 </template>
 
 <script>
 import Stats from './components/Stats';
-
+import Map from './components/Map';
 export default {
   name: 'App',
 
   components: {
     Stats,
+    Map
   },
 
   data: () => ({
---
+++ snow-finder/src/components/Map.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="map" ref="mapElement">
+  </div>
+</template>
+
+<script>
+import L from 'leaflet';
+
+export default {
+  name: "Map",
+  mounted() {
+    this.initMap();
+  },
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+  methods: {
+    initMap() {
+        let map = L.map(this.$refs['mapElement']).setView([42.204518, -74.210525], 13);
+        L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+          attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
+          minZoom: 1,
+          maxZoom: 19
+        }).addTo(map);
+
+        L.marker([42.204518, -74.210525]).addTo(map)
+          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+          .openPopup();
+      }
+    }
+}
+</script>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 100%;
+}
+</style>
