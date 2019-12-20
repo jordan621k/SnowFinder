ebc2c3a015e7eca53263861309b3eb802014d307
yomancool <tp65l4m06cl3@gmail.com>
2019-12-20 11:23:50 -0500

    fix button call

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -16,49 +16,36 @@ export default {
   },
   data() {
     return {
-      huntermtview: false,
-      mtcreekview: false,
-      camalbackview: false,
+      map: {}
     };
   },
   created() {
   },
   watch: {
-    huntermtview: {
-      immediate: true,
-      handler() {
-        this.HunterMtview()
-      }
-    }
   },
   methods: {
     initMap() {
-      let map = L.map(this.$refs['mapElement']).setView([41.17878, -74.53104], 7);
+      this.map = L.map(this.$refs['mapElement']).setView([41.17878, -74.53104], 7);
       L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
         attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
         minZoom: 1,
         maxZoom: 19
-      }).addTo(map);
+      }).addTo(this.map);
 
-      L.marker([42.204518, -74.210525]).addTo(map)
+      L.marker([42.204518, -74.210525]).addTo(this.map)
         .bindPopup('Hunter Mt')
         .openPopup();
       
-      L.marker([41.17878,-74.53104]).addTo(map)
+      L.marker([41.17878,-74.53104]).addTo(this.map)
         .bindPopup('Mt Creek')
         .openPopup();
       
-      L.marker([41.053379,-75.354263]).addTo(map)
+      L.marker([41.053379,-75.354263]).addTo(this.map)
         .bindPopup('Camalback')
         .openPopup();
-      //L.marker([42.204518, -75.210525]).addTo(map)
-        //.bindPopup('jordan')
-        //.openPopup();
     },
     HunterMtview() {
-      //console.log("xxx")
-      //this.Map = L.setView([42.204518, -74.210525], 13)
-      L.marker([42.204518, -75.210525]).addTo(this.initMap)
+      L.marker([42.204518, -75.210525]).addTo(this.map)
         .bindPopup('jordan')
         .openPopup();
     }
